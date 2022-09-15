361f1cd405c8ae369ec79e849cdd42d6a52e70aa
PC Correa <PC Correa@DESKTOP-Q9TQIF7>
2022-09-14 22:23:51 -0500

    Agregar archivos de proyecto.

---
+++ Nachopera.Division/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nachopera.Division
+{
+    public class Division
+    {
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <param name="i"></param>
+       /// <param name="j"></param>
+       /// <returns></returns>
+        public static long Dividir(long i, long j)
+        {
+            return (i / j);
+        }
+    }
+}
